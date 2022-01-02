56139aaa0bd5536d75252e553fbe8a91e870468c
Martin René Johansen <mrj@intelligentsystems.dk>
2022-01-02 18:01:25 +0100

    First version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,153 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ShotControl';
+
+  prepareTime$: number = +(localStorage.getItem('prepareTime') || 20);
+  shouldAnnounce$: boolean = (localStorage.getItem('announceTime') === 'true') || true;
+  announceTime$: number = +(localStorage.getItem('announceTime') || 3);
+  drawTime$: number = +(localStorage.getItem('drawTime') || 5.5);
+  shotPercentage$: number = +(localStorage.getItem('shotPercentage') || 75);
+
+  stage: 'Prepare' | 'Draw' | 'Stopped' = 'Stopped';
+
+  action: 'Shoot' | 'Let down' | undefined;
+
+  timer: any;
+  countDown: number | undefined;
+
+  getReadySound: HTMLAudioElement;
+  drawSound: HTMLAudioElement;
+  shootSound: HTMLAudioElement;
+  dontShootSound: HTMLAudioElement;
+
+  constructor() {
+    this.getReadySound = new Audio();
+    this.getReadySound.src = "./assets/sounds/get_ready.mp3";
+    this.getReadySound.load();
+
+    this.drawSound = new Audio();
+    this.drawSound.src = "./assets/sounds/draw.mp3";
+    this.drawSound.load();
+
+    this.shootSound = new Audio();
+    this.shootSound.src = "./assets/sounds/shoot.mp3";
+    this.shootSound.load();
+
+    this.dontShootSound = new Audio();
+    this.dontShootSound.src = "./assets/sounds/dont_shoot.mp3";
+    this.dontShootSound.load();
+  }
+
+  get prepareTime(): number {
+    return this.prepareTime$;
+  }
+
+  set prepareTime(value: number) {
+    this.prepareTime$ = value;
+
+    localStorage.setItem('prepareTime', '' + value)
+  }
+
+  get announceTime(): number {
+    return this.announceTime$;
+  }
+
+  set announceTime(value: number) {
+    this.announceTime$ = value;
+
+    localStorage.setItem('announceTime', '' + value)
+  }
+
+  get shouldAnnounce(): boolean {
+    return this.shouldAnnounce$;
+  }
+
+  set shouldAnnounce(value: boolean) {
+    this.shouldAnnounce$ = value;
+
+    localStorage.setItem('shouldAnnounce', '' + value)
+  }
+
+  get drawTime(): number {
+    return this.drawTime$;
+  }
+
+  set drawTime(value: number) {
+    this.drawTime$ = value;
+
+    localStorage.setItem('drawTime', '' + value)
+  }
+
+  get shotPercentage(): number {
+    return this.shotPercentage$;
+  }
+
+  set shotPercentage(value: number) {
+    this.shotPercentage$ = value;
+
+    localStorage.setItem('shotPercentage', '' + value)
+  }
+
+  doStart() {
+    if (this.stage === 'Stopped') {
+      this.stage = 'Prepare';
+
+      this.startTimer();
+    }
+  }
+
+  doStop() {
+    this.stage = 'Stopped';
+
+    if (this.timer) {
+      clearTimeout(this.timer);
+      this.timer = undefined;
+      this.action = undefined;
+      this.countDown = undefined;
+    }
+  }
+
+  startTimer() {
+    if (this.stage === 'Stopped') {
+      return;
+    }
+
+    if (this.stage === 'Prepare' && (this.countDown === undefined || this.countDown > 1)) {
+      if (this.countDown === undefined) {
+        this.countDown = this.prepareTime;
+      } else {
+        this.countDown--;
+      }
+
+      if (this.countDown === (this.prepareTime - 3)) {
+        this.action = undefined;
+      }
+
+      if (this.shouldAnnounce$ && this.countDown === this.announceTime) {
+        this.getReadySound.play();
+      }
+
+      this.timer = setTimeout(() => this.startTimer(), 1000);
+    } else {
+      this.stage = 'Draw';
+      this.countDown = undefined;
+
+      this.drawSound.play();
+      this.timer = setTimeout(() => {
+        const shotNumber = 100 * Math.random();
+
+        this.action = shotNumber < this.shotPercentage ? 'Shoot' : 'Let down';
+
+        if (this.action === 'Shoot') {
+          this.shootSound.play();
+        } else {
+          this.dontShootSound.play();
+        }
+
+        this.stage = 'Prepare';
+
+        this.startTimer();
+      }, this.drawTime * 1000);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule, ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCheckboxModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
